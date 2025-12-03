fd24d034a315467fe76db856a7d64881eb41a471
sighman <g00286398@atu.ie>
2025-12-03 11:43:01 +0000

    Initial commit

---
+++ src/main/java/ie/atu/proxyservice/ProxyserviceApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.proxyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProxyserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProxyserviceApplication.class, args);
+    }
+
+}
