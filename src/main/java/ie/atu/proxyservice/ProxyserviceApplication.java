8fc679138ca9234ded938324e543f4c56b6c5ac9
sighman <g00286398@atu.ie>
2025-12-03 11:58:01 +0000

    Adding git actions and Dockerfile to project

--- src/main/java/ie/atu/proxyservice/ProxyserviceApplication.java
+++ src/main/java/ie/atu/proxyservice/ProxyserviceApplication.java
@@ -2,8 +2,10 @@ package ie.atu.proxyservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class ProxyserviceApplication {
 
     public static void main(String[] args) {
